a8712b1b4442e878438ec200caf73ea1af98108e
arushi1947 <136613368+arushi1947@users.noreply.github.com>
2025-12-28 16:08:20 +0530

    Update ingestion_agent.py

--- ingestion_agent.py
+++ ingestion_agent.py
@@ -4,9 +4,7 @@ import os
 DATA_DIR = "data"
 
 def load_daily_reviews(target_date):
-    """
-    Loads reviews for a given date (YYYY-MM-DD)
-    """
+    
     file_path = os.path.join(DATA_DIR, f"{target_date}.json")
 
     if not os.path.exists(file_path):
@@ -15,3 +13,4 @@ def load_daily_reviews(target_date):
 
     with open(file_path, "r", encoding="utf-8") as f:
         return json.load(f)
+
